b518b5279a4a3cca92949c2eeeb470ccc9cab85c
finganapre <finganapre@gmail.com>
2018-05-15 17:43:37 +0500

    slider good

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,21 @@
 $(function(){
 	//new WOW().init();
+
 	$('.MainSlider').slick({
 		dots: true,
 		infinite: true,
-		speed: 300,
+		speed: 1000,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		//autoplay: true,
+		autoplay: true,
   		autoplaySpeed: 5000,
   		adaptiveHeight: true
 	});
+
+	$('.slick-dots');
+
+	window.FontAwesomeConfig = {
+    	searchPseudoElements: true
+  	}
 });
 
