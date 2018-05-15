c42d53633441df9a4df10ffabc661c7b97ca2eaf
finganapre <finganapre@gmail.com>
2018-05-16 02:10:54 +0500

    some adaptive... sub-navigation block ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+	//////////////////// LIBRARY INIT ////////////////////
+
 	//new WOW().init();
 
 	$('.MainSlider').slick({
@@ -12,10 +14,72 @@ $(function(){
   		adaptiveHeight: true
 	});
 
-	$('.slick-dots');
-
 	window.FontAwesomeConfig = {
     	searchPseudoElements: true
   	}
+
+
+
+
+
+
+
+
+
+
+  	//////////////////// CUSTOM JS ////////////////////
+
+  	// ---------- change MainActionBtns position ---------- //
+
+  	// ----- onLoad ----- //
+
+  	// variables declaration
+  	var lg = 1200;
+	var md = 992;
+	var sm = 768;
+
+	var windowWidth = 0;
+
+	var mainActionBtns = $('#main-action-btns');
+	var navigationLine = $('.navigation-line');
+	var mainHeader = $('.MainHeader');
+
+	var navLineClass = 'main-action-btns--navLine';
+
+	// test resolution and button position
+	changeActionBtnsPosCtrl();
+
+
+
+
+
+	// ----- onResize ----- //
+	window.addEventListener('resize', changeActionBtnsPosCtrl);
+
+
+
+
+
+	// ----- FUN ----- //
+
+	// test resolution and button position
+	function changeActionBtnsPosCtrl (event) {
+		windowWidth = document.documentElement.clientWidth;
+		window.removeEventListener('resize', changeActionBtnsPosCtrl);
+
+		(windowWidth < md) ? putMainBtnToNavLine() : putMainBtnToHeader();
+
+		window.addEventListener('resize', changeActionBtnsPosCtrl);
+	}
+
+	function putMainBtnToNavLine () {
+		mainActionBtns.detach().addClass(navLineClass);
+		navigationLine.prepend(mainActionBtns);
+	}
+
+	function putMainBtnToHeader () {
+		mainActionBtns.detach().removeClass(navLineClass);
+		mainHeader.find('.row').find('div').last().prepend(mainActionBtns);
+	}
 });
 
